56384372b4cc0ff51aba4c83227481e77b2a220a
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2024-09-27 20:01:40 -0600

    Conexión al front
    Add, se añade conexión con formulario

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');  // Importar cors
 const app = express();
 const port = 3000;
 
 const db = require('./db/connection');
+
+app.use(cors());
 
 app.use(express.json());
 
---
+++ script.js
@@ -0,0 +1,53 @@
+document.getElementById('formulario').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+
+    const nombre = document.getElementById('fname').value;
+    const apellido = document.getElementById('lname').value;
+    const carnet = document.getElementById('carnet').value;
+    const curso = document.getElementById('curso').value;
+    const nota = document.getElementById('nota').value;
+
+    try {
+        const estudianteResponse = await fetch('http://localhost:3000/estudiantes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nombre: nombre,
+                apellido: apellido,
+                carnet: carnet
+            })
+        });
+
+        const estudianteData = await estudianteResponse.json();
+
+        if (estudianteResponse.ok) {
+            const estudianteId = estudianteData.estudianteId;
+
+            const cursoResponse = await fetch('http://localhost:3000/cursos-estudiantes', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    estudiante_id: estudianteId,
+                    curso: curso,
+                    nota: nota
+                })
+            });
+
+            if (cursoResponse.ok) {
+                alert('Estudiante y curso insertados exitosamente');
+            } else {
+                alert('Error al insertar el curso');
+            }
+        } else {
+            alert('Error al insertar el estudiante');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Error al conectar con el servidor');
+    }
+});
